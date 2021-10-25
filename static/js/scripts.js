48816c13505173cb42e70c994023299bc28927a5
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-10-25 23:10:50 +0100

    Start adding mobile modal

---
+++ static/js/scripts.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+
+// Get Modal
+var modal = document.getElementById('myModal');
+    
+// Get pseudoelement to open Modal
+var btn = document.getElementById("sized");
+
+// Get the <span> element to close Modal
+var span = document.getElementsByClassName("close")[0];
+
+// When user clicks button, open Modal
+btn.onclick = function() {
+   modal.style.display = "block";
+   };
+
+// When user clicks Close (x), close Modal
+span.onclick = function() {
+   modal.style.display = "none";
+   };
+
+// When user clicks anywhere outside of the Modal, close Modal
+ window.onclick = function(event) {
+    if (event.target == modal) {
+       modal.style.display = "none";
+       }
+    }
+};
