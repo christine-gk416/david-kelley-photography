284d8107d71772e77bdb4621d6e8e0de1984668c
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-08 21:51:40 +0000

    Add delete function for products

--- products/views.py
+++ products/views.py
@@ -54,21 +54,21 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
-        'on_addproduct_page': True
     }
 
     return render(request, template, context)
+
 
 
 def edit_product(request, product_id):
@@ -93,3 +93,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
