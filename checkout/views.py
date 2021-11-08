0dd5c031f8e3090d4b4a313b8c501a3adb96ff95
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-08 10:02:01 +0000

    Add country dropdown to checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -22,11 +22,11 @@ def checkout(request):
             'email': request.POST['email'],
             'phone_number': request.POST['phone_number'],
             'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
             'town_or_city': request.POST['town_or_city'],
             'street_address1': request.POST['street_address1'],
             'street_address2': request.POST['street_address2'],
-            'county': request.POST['county'],
+            'state': request.POST['state'],
+            'postcode': request.POST['postcode'], 
         }
         order_form = OrderForm(form_data)
 
@@ -73,11 +73,11 @@ def checkout(request):
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+    # stripe.api_key = stripe_secret_key
+    # intent = stripe.PaymentIntent.create(
+    #     amount=stripe_total,
+    #     currency=settings.STRIPE_CURRENCY,
+    # )
 
     order_form = OrderForm()
 
@@ -89,7 +89,7 @@ def checkout(request):
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        # 'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
