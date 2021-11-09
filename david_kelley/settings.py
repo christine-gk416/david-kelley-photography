0889f8c1f72e3aa7bbbfdffd4f7bf0b19d22f613
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-09 20:11:32 +0000

    Adjust path location for aws

--- david_kelley/settings.py
+++ david_kelley/settings.py
@@ -188,6 +188,7 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_ADDRESSING_STYLE = "path"
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
