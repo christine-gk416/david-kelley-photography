337dfb3a64db930cdf71670e586b2112f4a9eb74
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-09 00:15:50 +0000

    Add new secret key for django

--- david_kelley/settings.py
+++ david_kelley/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hp5br69pm$faqs91ke&+i%bma3p_3ueaooj=6(lhzr%to!(a7a'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['david-kelley-photography.herokuapp.com', 'localhost']
 
