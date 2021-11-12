c3d45ddd5e3740cf1787ea8a7844815be2e1c571
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-12 15:53:46 +0000

    Update styling on products pages and products forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,15 @@
 from .models import Comment, Post
 from django import forms
-# from .widgets import CustomClearableFileInput
-
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = '__all__'
 
-    # image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'contact-border rounded-0'
 
 class CommentForm(forms.ModelForm):
     class Meta:
