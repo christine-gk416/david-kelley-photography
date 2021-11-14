2de9dba4ab87fadf1d060f58bacfb59319b43281
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-14 21:35:28 +0000

    Test live checkout page

--- david_kelley/settings.py
+++ david_kelley/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True 
 
 ALLOWED_HOSTS = ['david-kelley-photography.herokuapp.com', 'localhost']
 
