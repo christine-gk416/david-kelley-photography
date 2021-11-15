f1c7f3cc64d363deb6f4640c05716c1470b2cdca
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-15 23:27:30 +0000

    Update contact form

--- david_kelley/settings.py
+++ david_kelley/settings.py
@@ -227,6 +227,7 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
 
 # Twilio SendGrid
 
