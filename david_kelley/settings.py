ac31346a7abc64cb0c4ed21f51f60e09d820ce91
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-15 12:59:12 +0000

    Update email configurations

--- david_kelley/settings.py
+++ david_kelley/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True 
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True 
 
 ALLOWED_HOSTS = ['david-kelley-photography.herokuapp.com', 'localhost']
 
