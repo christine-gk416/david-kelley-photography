e4e1b053a3fc30eadd48f56dc72679f9e8d3ac4a
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-15 13:10:16 +0000

    Disable sendgrid info to test gmail

--- david_kelley/settings.py
+++ david_kelley/settings.py
@@ -230,11 +230,11 @@ else:
 
 # Twilio SendGrid
 
-CONTACT_EMAIL = 'cgk416@gmail.com'
-ADMIN_EMAIL = ['cgk416@gmail.com', ]
+# CONTACT_EMAIL = 'cgk416@gmail.com'
+# ADMIN_EMAIL = ['cgk416@gmail.com', ]
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
