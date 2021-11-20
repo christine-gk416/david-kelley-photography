5771ca37184661f7d2191f42f800c99c9f12bc89
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-20 21:27:17 +0000

    Set debug to False

--- david_kelley/settings.py
+++ david_kelley/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['david-kelley-photography.herokuapp.com', 'localhost']
 
