4c14633799d0c79d0bee865b3f50035234425c07
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-11-20 21:36:07 +0000

    Test out 404 and 500 pages

--- david_kelley/urls.py
+++ david_kelley/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = "home.views.error_404"
-handler500 = "home.views.error_500"
+# handler404 = "home.views.error_404"
+# handler500 = "home.views.error_500"
